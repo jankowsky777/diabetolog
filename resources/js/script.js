f97da3f785a35b6b71dffc0720cb3e92651d3bfa
jankowsky777 <maciej@jankowsky.pl>
2019-02-05 10:10:51 +0100

    repo of diabetolog project

---
+++ resources/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+//  Sticky navigation trigger
+
+    $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px;'
+    });
+
+//  Navigation scrollto
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+    
+
+
+})
